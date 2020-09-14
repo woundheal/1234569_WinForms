3b0f0547a2050856ddc9f8959937543dc58b5553
woundheal <70649882+woundheal@users.noreply.github.com>
2020-09-14 16:00:06 -0500

    Form1 name change to FromMain

--- 1234569_WinForms/FormMain.Designer.cs
+++ 1234569_WinForms/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _1234569_WinForms
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
--- 1234569_WinForms/Program.cs
+++ 1234569_WinForms/Program.cs
@@ -16,7 +16,7 @@ namespace _1234569_WinForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
